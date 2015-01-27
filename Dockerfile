9441e7f7cb95ae11036e52bae1231fa86e853033
Brian DeHamer <brian@dehamer.com>
2015-01-27 14:47:26 -0800

    Golang 1.4.1 update
    Also simplifies usage by eliminating the need to explicitly specify the
    package name.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM google/golang
+FROM golang:1.4
 
 # Install Docker binary
 RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y wget
 RUN wget -nv https://get.docker.com/builds/Linux/x86_64/docker-1.3.3 -O /usr/bin/docker && \
   chmod +x /usr/bin/docker
 
-VOLUME ["/src"]
+VOLUME /src
+WORKDIR /src
 
 COPY build.sh /
 
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PKG_NAME=$1
-TAG_NAME=$2
+tagName=$1
 
 if ( find /src -maxdepth 0 -empty | read v );
 then
@@ -9,38 +8,44 @@ then
   exit 990
 fi
 
-if [ -z "$PKG_NAME" ];
-then
-  echo "Error: Must specify Go package to be compiled"
-  exit 991
-fi
-
 if ! [ -e "/var/run/docker.sock" ];
 then
   echo "Error: Docker socket must be mounted at /var/run/docker.sock"
+  exit 991
+fi
+
+# Grab Go package name
+pkgName="$(go list -e -f '{{.ImportComment}}' 2>/dev/null || true)"
+
+if [ -z "$pkgName" ];
+then
+  echo "Error: Must add canonical import path to root package"
   exit 992
 fi
 
+# Grab just first path listed in GOPATH
+goPath="${GOPATH%%:*}"
+
+# Construct Go package path
+pkgPath="$goPath/src/$pkgName"
+
 # Set-up src directory tree in GOPATH
-mkdir -p /gopath/src/$PKG_NAME
+mkdir -p "$(dirname "$pkgPath")"
 
-# Copy mounted source code into GOPATH
-cp -r /src/* /gopath/src/$PKG_NAME
-
-# Change to package directory
-cd /gopath/src/$PKG_NAME
+# Link source dir into GOPATH
+ln -sf /src "$pkgPath"
 
 # Get all package dependencies
 go get -d -v ./... 
 
 # Compile statically linked version of package
-CGO_ENABLED=0 go build -a -ldflags '-s' $PKG_NAME
+CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags '-s' $pkgName
 
 # Grab the last segment from the package name
-NAME=${PKG_NAME##*/}
+name=${pkgName##*/}
 
 # Default TAG_NAME to package name if not set explicitly
-TAG_NAME=${TAG_NAME:-"$NAME":latest}
+tagName=${tagName:-"$name":latest}
 
 # Build the image from the Dockerfile in the package directory
-docker build -t $TAG_NAME .
+docker build -t $tagName .
