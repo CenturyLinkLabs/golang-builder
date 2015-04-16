335d8bf9bf3f4570ed2f7af603a972ef5b4ed1fd
Don Petersen <don@donpetersen.net>
2015-04-16 21:39:36 +0000

    Remove a few build flags that I'm unsure about.
    My reading about cross-compilation in Go tells me that it's using CGO whether
    you like it or not. Allowing it to be disabled just produces binaries that
    don't work on other platforms. I'm unsure about the installsuffix and ldflags,
    and so I'm removing them to not give somebody a false sense that they might
    work. Maybe they do, and if that person has a way to test their working or not,
    they can add it back. Sorry theoretical future person.

--- builder-cross/build.sh
+++ builder-cross/build.sh
@@ -8,13 +8,9 @@ name=${pkgName##*/}
 for GOOS in darwin linux; do
         for GOARCH in 386 amd64; do
                 echo "Building $name for $GOOS-$GOARCH"
-                # Compile statically linked version of package
-                `CGO_ENABLED=${CGO_ENABLED:-0} go build \
+                `go build \
                         -v \
                         -o $name-$GOOS-$GOARCH \
-                        -a \
-                        --installsuffix cgo \
-                        --ldflags="${LDFLAGS:--s}" \
                         $pkgName`
                 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
         done
