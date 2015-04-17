20a53b9dc467a638107d3afbbcba186d64f0f146
Don Petersen <don@donpetersen.net>
2015-04-17 19:57:37 +0000

    Fix GOOS/GOARCH variables not being present in cross.
    See comment. So annoying.

--- builder-cross/build.sh
+++ builder-cross/build.sh
@@ -8,7 +8,12 @@ name=${pkgName##*/}
 for GOOS in darwin linux; do
         for GOARCH in 386 amd64; do
                 echo "Building $name for $GOOS-$GOARCH"
-                `go build \
+                # Why am I redefining the same variables that already existed?
+                # Somehow they're not available just from the loop, unless I
+                # either export them or do this. My theory is that it's somehow
+                # building in another process that doesn't have access to the
+                # loop variables. That caused everything to be built for linux.
+                `GOOS=$GOOS GOARCH=$GOARCH go build \
                         -v \
                         -o $name-$GOOS-$GOARCH \
                         $pkgName`
