f132ee06ace7e081964765f1cf5d00710efef970
Christopher 'Chief' Najewicz <chief@beefdisciple.com>
2015-11-10 09:51:41 -0500

    Add OUTPUT optional env var to use go build's -o option to output final binary to custom location. Update to docker 1.9.0

--- builder/build.sh
+++ builder/build.sh
@@ -2,19 +2,43 @@
 
 source /build_environment.sh
 
-# Compile statically linked version of package
-echo "Building $pkgName"
-`CGO_ENABLED=${CGO_ENABLED:-0} go build -a --installsuffix cgo --ldflags="${LDFLAGS:--s}" $pkgName`
-
 # Grab the last segment from the package name
 name=${pkgName##*/}
 
-if [[ $COMPRESS_BINARY == "true" ]];
+#
+# Optional OUTPUT env var to use the "-o" go build switch
+# forces build to write the resulting executable or object
+# to the named output file
+#
+output=""
+if [[ ! -z "${OUTPUT}" ]];
 then
-  goupx $name
+  output="-o ${OUTPUT}"
 fi
 
-if [ -e "/var/run/docker.sock" ] && [ -e "./Dockerfile" ];
+# Compile statically linked version of package
+echo "Building $pkgName"
+(
+  CGO_ENABLED=${CGO_ENABLED:-0} \
+  go build \
+  -a \
+  ${output} \
+  --installsuffix cgo \
+  --ldflags="${LDFLAGS:--s}" \
+  $pkgName
+)
+
+if [[ "$COMPRESS_BINARY" == "true" ]];
+then
+  if [[ ! -z "${OUTPUT}" ]];
+  then
+    goupx ${OUTPUT}
+  else
+    goupx $name
+  fi
+fi
+
+if [[ -e "/var/run/docker.sock"  &&  -e "./Dockerfile" ]];
 then
   # Default TAG_NAME to package name if not set explicitly
   tagName=${tagName:-"$name":latest}
